b8e6b0ec89fc586598013518e22521785b3ff059
Muhammad Tayyab Shoukat <100590476+TayyabShoukat@users.noreply.github.com>
2024-07-11 21:59:43 +0500

    Variables

---
+++ variables with nextjs/my-app/src/app/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./page.module.css";
+export default function Page() {
+  const a: number = 3;
+  const b: number = 4;
+  const c: number = a + b;
+
+  const arr: number[] = [2, 4, 6, 8, 10];
+
+  const arr1: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  return (
+    <div>
+      <p>Sum is {c}</p>
+      <p>Subtract is {a - b}</p>
+      <p>Subtract is {a * b}</p>
+      <p>Subtract is {a / b}</p>
+
+      <div className={styles.d}>
+        {arr.map((item) => (
+          <p>{item}</p>
+        ))}
+      </div>
+
+      <div>
+        <p className={styles.p}>{arr1.filter((item) => item % 2 == 0)}</p>
+      </div>
+    </div>
+  );
+}
