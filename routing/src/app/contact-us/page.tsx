b1bb81a81d646235359fffccbb309e80e925747c
Muhammad Tayyab Shoukat <100590476+TayyabShoukat@users.noreply.github.com>
2024-07-23 21:20:55 +0500

    routing

---
+++ routing/src/app/contact-us/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./contactus.module.css";
+
+export default function Page() {
+  return (
+    <div>
+      <p className={styles.heading}>Contact Us</p>
+    </div>
+  );
+}
