3df502f7da55ad8bb57c2a792e9d1d3f8c8a163b
Muhammad Tayyab Shoukat <100590476+TayyabShoukat@users.noreply.github.com>
2024-07-24 22:10:22 +0500

    dynamic routing complete

--- dynamic routing/my-app/src/app/products/[slug]/page.tsx
+++ dynamic routing/my-app/src/app/products/[slug]/page.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import jsondata from "../../../../data.json";
+import Image from "next/image";
+import styles from "./products.module.css";
 
 export default function Page({ params }: any) {
+  const result = jsondata?.filter((item) => item?.slug === params?.slug);
+
   return (
     <div>
-      <h1>{params.slug}</h1>
+      {result?.map((item: any, index: any) => (
+        <div key={index} className={styles.container}>
+          <h1>{item?.slug}</h1>
+          <Image src={item?.src} alt="no image" height={800} width={800} />
+          <h3>{item?.description}</h3>
+        </div>
+      ))}
     </div>
   );
 }
